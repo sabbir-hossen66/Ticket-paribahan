acc7027b9337715e30cda23f542f3182265c2dde
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-02-19 02:21:25 +0600

    footer done & js connected

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,8 @@
-console.log('hello');
+const allSeatBtn = document.getElementsByClassName('seat-btn');
+// console.log(allSeatBtn);
+
+for (const btn of allSeatBtn) {
+  btn.addEventListener('click', function () {
+    console.log('mama');
+  })
+}
