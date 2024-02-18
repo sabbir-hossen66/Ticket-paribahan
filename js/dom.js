713f6c82857a9df401409e2e150f6dbdf3479865
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-02-19 02:34:55 +0600

    counting is start

--- js/dom.js
+++ js/dom.js
@@ -1,8 +1,10 @@
 const allSeatBtn = document.getElementsByClassName('seat-btn');
 // console.log(allSeatBtn);
 
+let count = 0;
 for (const btn of allSeatBtn) {
-  btn.addEventListener('click', function () {
-    console.log('mama');
+  btn.addEventListener('click', function (e) {
+    count = count + 1;
+    document.getElementById("seat-count").innerText = count;
   })
 }
