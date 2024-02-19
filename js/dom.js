7a4e727e18489010587186a4b7ebe11f598ea6de
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-02-19 06:30:05 +0600

    grandtotal is complete

--- js/dom.js
+++ js/dom.js
@@ -8,23 +8,34 @@ for (const btn of allSeatBtn) {
 
     // show the sitname on the screen
     const sitName = e.target.innerText;
+    const price = 550;
     const showSitName = document.getElementById('show-sit-name');
     const li = document.createElement('li');
 
     const p = document.createElement('p');
+    const p2 = document.createElement('p');
     p.innerText = sitName;
+    p2.innerText = price;
+
     li.appendChild(p);
     showSitName.appendChild(p);
+    showSitName.appendChild(p2);
 
     // total cost shows
-    const price = 550;
     const totalCost = document.getElementById('total-price').innerText;
 
-    const convertedTotalCost = parseInt(totalCost);
-    document.getElementById('total-price').innerText =
-      convertedTotalCost + parseInt(price)
+    const convertedTotalCost = parseInt(totalCost)
+    const sum = convertedTotalCost + parseInt(price)
 
 
+    // GRAND TOTAL  
+    const grandTotal = document.getElementById('grand-total').innerText;
+    const convertedGrandTotal = parseInt(grandTotal);
+    const sumOfGrand = convertedGrandTotal + parseInt(price);
+
+
+    innerText('grand-total', sumOfGrand)
+    innerText('total-price', sum)
     innerText('seat-count', count)
   })
 }
